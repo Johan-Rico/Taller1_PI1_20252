c80b3d5d0ae43a5b57faf1bce0ffef4de3824a4e
Johan-Rico <jsricon@eafit.edu.co>
2025-07-31 10:47:03 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse("<h1>Johan Samuel Rico Nivia</h1>")
+    return render(request, 'home.html', {'name': 'Johan Samuel Rico Nivia'})
+
+def about(request):
+    return HttpResponse("<h1>Welcome to the About Page</h1>")
