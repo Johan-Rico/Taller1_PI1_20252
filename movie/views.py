3fbf6e0d036faeba0abe8ca995b73e538c4cfe90
Johan-Rico <jsricon@eafit.edu.co>
2025-07-31 11:12:54 -0500

    uso de plantilla about.html

--- movie/views.py
+++ movie/views.py
@@ -4,8 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    #return HttpResponse("<h1>Johan Samuel Rico Nivia</h1>")
     return render(request, 'home.html', {'name': 'Johan Rico'})
 
 def about(request):
-    return HttpResponse("<h1>Welcome to the About Page</h1>")
+    return render(request, 'about.html')
